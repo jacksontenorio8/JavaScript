41de315ba9d09c7dc1e3bf0163fa44ca2a9e78dd
Jackson <jacksontenorio8@gmail.com>
2025-11-14 17:28:37 -0300

    Update em app.js
    Foi acresentado a lógica while.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,13 +4,17 @@ let numeroSecreto = 5;
 console.log("numeroSecreto");
 let chute = prompt("Escolha um número entre 0 e 10: ");
 
-// Se o chute for igual ao número secreto
-if (chute == numeroSecreto){
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}.`);
-} else {
-    if (chute > numeroSecreto){
-        alert(`O número secreto é menor que ${chute}.`);
+// Enquanto o chute for diferente do número secreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 10: ');
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto){
+            alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}!`);
     } else {
-        alert(`O número secreto é maior que ${chute}.`);
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}.`);
+        } else{
+            alert(`O número secreto é maior que ${chute}.`);
+        }
     }
 }
